231fb6ede6cd5ae1b6bd173ac74dd0de1aa50572
Yukihiro Okuno <landmarkmonster1234@gmail.com>
2022-11-27 23:19:36 +0900

    アプリケーションファクトリの解除

--- app/__init__.py
+++ app/__init__.py
@@ -2,21 +2,20 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 
+app = Flask(__name__)
+app.config.from_object('app.config')
+
 db = SQLAlchemy()
+db.init_app(app)
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object('app.config')
-    db.init_app(app)
-    
-    from app.views.users import user
-    app.register_blueprint(user)
-    
-    from app.views.entries import entry
-    app.register_blueprint(entry)
-    
-    from app.config import Base, engine 
-    Base.metadata.create_all(engine)
+from app.views.users import user
+app.register_blueprint(user)
 
-    return app
+from app.views.entries import entry
+app.register_blueprint(entry)
+
+from app.config import Base, engine 
+Base.metadata.create_all(engine)
+
+
 
